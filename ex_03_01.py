dd8270bb61333cb6afc917bdf57b6a3d3f72a54b
huangjianseven <huangjian7@gmail.com>
2020-11-10 06:55:21 +0800

    add try/except to capture traceback

--- ex_03_01.py
+++ ex_03_01.py
@@ -2,13 +2,16 @@ hrs = input("Enter Hours:")
 
 rate = input("Enter Hourly Rate:")
 
-hrs = float(hrs)
-rate = float(rate)
 
-if hrs > 40:
-	rate_new = rate * 1.5
-	pay = (hrs-40)*rate_new +40*rate 
-else:
-	pay = hrs * rate
+try:
+    hrs = float(hrs)
+    rate = float(rate)
 
-print(pay)
+    if hrs > 40:
+        rate_new = rate * 1.5
+        pay = (hrs-40)*rate_new +40*rate 
+    else:
+        pay = hrs * rate
+    print(pay)
+except:
+    print("Please input number correctly")
